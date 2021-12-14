b711056439f94b56b406fe176bab17d69ffe73c0
RajrupDasid <rajrupdasid@outlook.com>
2021-12-14 08:07:27 +0530

    main file rectified and added

---
+++ main.py
@@ -0,0 +1,42 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
+
+app=FastAPI()
+
+class Post(BaseModel):
+    title:str
+    content:str
+    published:bool=True
+    rating:Optional[int]=None
+
+
+
+
+
+
+@app.get("/")
+async def root():
+    return {"message":"Hell YEAH!- API Running"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data":"This is your post"}
+
+@app.post("/createposts")
+def create_posts(new_post:Post):
+    print(new_post)
+    print(new_post.dict())
+    return {"data":"new_post"}
+
+
+
+
+#def create_posts(payload: dict = Body(...)):
+ #   print(payload)
+   # return {"Message":"Post Successfully created"}
+  #  return {"new_posts":f"title{payload['title']} content: {payload['content']}"}
+
+
