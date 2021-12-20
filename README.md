81ed6d211a791dd4d38ed65f997fd4f11265f5b5
RajrupDasid <rajrupdasid@outlook.com>
2021-12-20 18:08:18 +0530

    CRUD logic written and rectified issues just some general polish needed

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 This project is the fully flow of a new and easy python based API or Application Programming  Interface called Fast-API 
-you don't have to understand sql quaries and some other complex process bild upon simple python concepts 
+you don't have to understand sql quaries and some other complex process build upon simple python concepts 
 This Project is available both Gitlab and Bitbucket
 Later I will update it on Github
+
+for documentation FAST-API has built-in features  combined with Swagger UI if you went to path local host and then /docs it will gives an amazing looking documentation 
+FAST-API is really fast for every where 
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI,Response,status,HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 
+
 app=FastAPI()
 
+#pydantic model
 class Post(BaseModel):
     title:str
     content:str
@@ -13,21 +15,42 @@ class Post(BaseModel):
     rating:Optional[int]=None
 
 
+#simple structures 
+#def create_posts(payload: dict = Body(...)):
+ #   print(payload)
+   # return {"Message":"Post Successfully created"}
+  #  return {"new_posts":f"title{payload['title']} content: {payload['content']}"}
 
 
 #global variable decleared array to hold posts in a list in memory
 my_posts=[{"title":"title post 1", "content":"content 1", "id":1},{"title":"favorite foods","content":"I like Pizza", "id":2}]
 
+def find_post(id):
+    for p in my_posts:
+        if p ["id"]==id:
+            return p
+
+def find_index_post(id):
+    for i,p  in enumerate(my_posts):
+        if p['id']==id:
+            return i
+
+#redirection paths 
+
+#simple message on main page
 @app.get("/")
 async def root():
     return {"message":"Hell YEAH!- API Running"}
 
+
+#router for getting all posts
 @app.get("/posts")
 def get_posts():
     return {"data":my_posts}
     #return {"data":"This is your post"}
 
-@app.post("/posts")
+#router for create posts
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_posts(post:Post):
     print(post)
     print(post.dict())
@@ -36,13 +59,39 @@ def create_posts(post:Post):
     my_posts.append(post_dict)
     return {"data":post_dict}
 
+#router for posts with specific id
+@app.get("/posts/{id}") #id refrenced as a path parameter
+def get_post(id:int,response:Response):
+    post=find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id:{id} not found")
+    return {"post_details":post}
+        #unused code
+        #response.status_code=status.HTTP_404_NOT_FOUND
+        #return {'message':f"post with id:{id} not found"}
+    #return{"Post Details":f"Here is the ID of the post+'{id}'"}
+    
 
 
+#delete posts 
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id:int):
+    index=find_index_post(id)
+    if index==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id :{id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
+    #return {'Message':'post was successfully deleted'}
 
-#def create_posts(payload: dict = Body(...)):
- #   print(payload)
-   # return {"Message":"Post Successfully created"}
-  #  return {"new_posts":f"title{payload['title']} content: {payload['content']}"}
+#update a posts
+@app.put("/posts/{id}")
+def update_posts(id:int,post:Post):
+    index=find_index_post(id)
+    if index==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id :{id} does not exist")
+    post_dict=post.dict()
+    post_dict['id']=id 
+    my_posts[index]=post_dict   
 
-
+    return {"data":post_dict}
