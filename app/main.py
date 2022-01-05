f1be8d49c8e02ca0f6c9cd325ff46b827ce386e8
RajrupDasid <rajrupdasid@outlook.com>
2022-01-05 20:11:28 +0530

    CRUD operation hasbeen performed using traditional SQL method

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ class Post(BaseModel):
 while True:
     try:
         conn=psycopg2.connect(host='localhost',database='fastapi',port='5432',user='postgres',password='passkey',cursor_factory=RealDictCursor)
-        cursor=conn.cursor
+        cursor=conn.cursor()
         print("Connected")
         break
     except Exception as e:
@@ -60,26 +60,45 @@ async def root():
 #router for getting all posts
 @app.get("/posts")
 def get_posts():
-    return {"data":my_posts}
+    cursor.execute("""SELECT * FROM posts""") #sql statements
+    posts=cursor.fetchall()
+    return {"data":posts}
+
+    #return {"data":my_posts}
     #return {"data":"This is your post"}
 
 #router for create posts
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_posts(post:Post):
-    print(post)
-    print(post.dict())
-    post_dict=post.dict()
-    post_dict['id']=randrange(0,1000000)
-    my_posts.append(post_dict)
-    return {"data":post_dict}
+    cursor.execute("""INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING *""",(post.title,post.content,post.published))#sanitizing data while inserting it
+    new_post=cursor.fetchone()
+    conn.commit()#stage changing to database it self
+    return {"data":new_post}
+
+    #print(post)
+    #print(post.dict())
+    #post_dict=post.dict()
+    #post_dict['id']=randrange(0,1000000)
+    #my_posts.append(post_dict)
+    #return {"data":post_dict}
+    
+
+
 
 #router for posts with specific id
 @app.get("/posts/{id}") #id refrenced as a path parameter
 def get_post(id:int,response:Response):
-    post=find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s """,(str(id)))
+    post=cursor.fetchone()
     if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id:{id} not found")
-    return {"post_details":post}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id:{id} was not found")
+    return{"data":post}
+
+
+    #post=find_post(id)
+    #if not post:
+     #   raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id:{id} not found")
+    #return {"post_details":post}
         #unused code
         #response.status_code=status.HTTP_404_NOT_FOUND
         #return {'message':f"post with id:{id} not found"}
@@ -90,10 +109,13 @@ def get_post(id:int,response:Response):
 #delete posts 
 @app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id:int):
-    index=find_index_post(id)
-    if index==None:
+    cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""",(str(id)))
+    deleted_posts=cursor.fetchone()
+    conn.commit()
+    #index=find_index_post(id)
+    if deleted_posts==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id :{id} does not exist")
-    my_posts.pop(index)
+    #my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
     #return {'Message':'post was successfully deleted'}
@@ -101,11 +123,14 @@ def delete_post(id:int):
 #update a posts
 @app.put("/posts/{id}")
 def update_posts(id:int,post:Post):
-    index=find_index_post(id)
-    if index==None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id :{id} does not exist")
-    post_dict=post.dict()
-    post_dict['id']=id 
-    my_posts[index]=post_dict   
+    cursor.execute("""UPDATE posts SET title = %s, content=%s,published=%s WHERE id=%s RETURNING *""",(post.title,post.content,post.published,str(id)))
+    post_dict=cursor.fetchone()
+    conn.commit()
 
+    #index=find_index_post(id)
+    if post_dict==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id :{id} does not exist")
+    #post_dict=post.dict()
+    #post_dict['id']=id 
+    #my_posts[index]=post_dict   
     return {"data":post_dict}
